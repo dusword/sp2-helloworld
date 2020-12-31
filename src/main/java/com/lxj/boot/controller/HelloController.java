1ebd7f2d8aeb04749dc7557ebee1ee38c3f3b9dc
dusword <773983534@qq.com>
2020-12-31 15:32:39 +0800

    尝试@RequestParam

--- src/main/java/com/lxj/boot/controller/HelloController.java
+++ src/main/java/com/lxj/boot/controller/HelloController.java
@@ -1,13 +1,14 @@
 package com.lxj.boot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String helloWorld(){
-        return "This is a SpringBoot2 Hello World!";
+    public String helloWorld(@RequestParam("name") String name){
+        return "This is a SpringBoot2 Hello World!"+name;
     }
 }
